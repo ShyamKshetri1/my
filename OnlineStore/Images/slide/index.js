6c84e8fed4341a8323eced5bde9b9acc074a6369
ShyamKumarKshetri0 <70106085+ShyamKshetri1@users.noreply.github.com>
2023-07-04 15:03:51 +0545

    Update index.js
    Code updated

--- OnlineStore/Images/slide/index.js
+++ OnlineStore/Images/slide/index.js
@@ -1,4 +1,38 @@
-var host ='https://shyamkshetri1.github.io/online_db/OnlineStore/Images/slide/banner';
+
+var a = 'h'+
+  't'+
+  't'+'p'
+  +'s';
+var b = a+':'+'//';
+var c = b+ 's'+'h'+
+  'y'+'a'+   'm'+
+  'k'+'s'
+  +'h'+
+  'e'+'t'+'r'
+  +'i'+'1'
+  +'.';
+var d = c+'gi'
+  +'t'+'hu'+
+  'b'+'.io';
+var e = d+'/onl'
+  +'in'+'e'+'_db';
+var f = e+'/On'+
+  'line'+
+  'Sto'+'re';
+var g = f+'/Ima'+'ges';
+var h = g+'/sli'
+  +'de';
+var _i = h+
+  '/ban'+
+  'ner';
+var j = '.pn'
+  +'g';
+
+result = '[';
 for(var i = 0; i<6; i++){
-console.log(host+i+'.png');
+result += '{\"img\":\"'+_i+i+j+'\"},\n';
 }
+
+result = result.substring(0,result.length-2)+']';
+
+console.log(result);
